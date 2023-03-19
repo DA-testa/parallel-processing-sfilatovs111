7bc55a23a20ef8902b0873def00bfe10c8bb8347
Sergejs 221RDB111 <125831670+sfilatovs111@users.noreply.github.com>
2023-03-20 00:18:59 +0200

    221RDB111v1

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 # python3
 
-def parallel_processing(n, m, data):
+def parallel_processing(n_threads, m_jobs, job_times):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
+    time_stamp = [0] * n_threads
+
+    for i in range(m_jobs):
+        next_thread = 0
+        for j in range(1, n_threads):
+            if time_stamp[j] < time_stamp[next_thread]:
+                next_thread = j
+            elif time_stamp[j] == time_stamp[next_thread] and j < next_thread:
+                next_thread = j
+
+        output.append((next_thread, time_stamp[next_thread]))
+        time_stamp[next_thread] += job_times[i]
+
+
 
     return output
 
+
+
+
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
+    n_threads, m_jobs = map(int, input().split())
+    job_times = list(map(int, input().split()))
 
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    result = parallel_processing(n_threads, m_jobs, job_times)
 
-    # TODO: create the function
-    result = parallel_processing(n,m,data)
-    
-    # TODO: print out the results, each pair in it's own line
+    for thread, start_time in result:
+        print(thread, start_time)
+
 
 
 
